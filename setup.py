0be8b03a1f8a47f2aeac84923864a626c5a6bdb2
Noriyuki Abe <noriyukipy@gmail.com>
2021-08-08 11:11:49 +0900

    Add libraries for training option

--- setup.py
+++ setup.py
@@ -5,10 +5,17 @@ setuptools.setup(
     name="convmodel",
     packages=setuptools.find_packages(),
     install_requires=[
-        "pydantic>=1.8,<2",
-        "transformers==4.8.2",
+        "pydantic>=1,<2",
+        "transformers>=4.8.2,<5",
     ],
     extras_require={
+        "train": [
+            "sentencepiece>=0.1.95,<0.2",
+            "pytorch-lightning>=1.4.1,<2",
+            # Install pytorch-lightning as well as jsonargparse for enabling LightningCLI
+            "jsonargparse[signatures]>=3.17.0,<4",
+            "fire>=0.4.0,<0.5",
+        ],
         "test": ["pytest>=5.0,<60", "black>=20.8b1,<21"],
     },
     version="0.0.0",
